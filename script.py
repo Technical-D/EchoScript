bf3acbf8b59d69d8f77209471099d7affe72b18a
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2025-02-04 14:38:24 +0530

    Initial Commit

---
+++ script.py
@@ -0,0 +1,28 @@
+import os
+from transcription import process_files
+
+def scan_media_files(folder_path):
+    # File extensions
+    audio_extensions = {".mp3", ".wav", ".flac", ".aac", ".ogg", ".m4a"}
+    video_extensions = {".mp4", ".mkv", ".avi", ".mov", ".wmv", ".flv"}
+
+    # Dict to store the file location
+    audio_files = {}
+    video_files = {}
+
+    for root, _, files in os.walk(folder_path):
+        for file in files:
+            file_name = os.path.splitext(file)[0].lower()
+            file_ext = os.path.splitext(file)[1].lower()
+            file_path = os.path.join(root, file)
+            
+            if file_ext in audio_extensions:
+                audio_files[file_name] = file_path
+            elif file_ext in video_extensions:
+                video_files[file_name] = file_path
+    return audio_files, video_files
+
+audio_files, video_files = scan_media_files('media')
+print("File extracted sucessfully!")
+print(audio_files)
+process_files(audio_files=audio_files, video_files=video_files, output_folder="transcriptions")
---
+++ transcription.py
@@ -0,0 +1,45 @@
+import whisper
+import os
+import ffmpeg
+
+def convert_video_to_audio(video_path, output_audio_path):
+    # Converting video file into audio for transcription
+    try:
+        ffmpeg.input(video_path).output(output_audio_path, format='mp3', acodec='mp3').run(quiet=True, overwrite_output=True)
+        return output_audio_path
+    except Exception as e:
+        print(f"Error converting {video_path} to audio: {e}")
+        return None
+
+def transcribe_audio(file_path, model):
+    print(f"Transcribing: {file_path} ...")
+    result = model.transcribe(file_path)
+    return result["text"]
+
+def save_transcription(file_name, text, output_folder):
+    output_file = os.path.join(output_folder, f"{file_name}.txt")
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(text)
+    print(f"Saved transcription: {output_file}")
+
+def process_files(audio_files, video_files, output_folder="transcriptions"):
+    # Creating a output directory
+    os.makedirs(output_folder, exist_ok=True)
+    model = whisper.load_model("tiny")
+
+    # looping through audio file
+    for file_name, file_path in audio_files.items():
+        print(file_path)
+        text = transcribe_audio(file_path, model)
+        save_transcription(file_name, text, output_folder)
+
+    # looping through video file
+    for file_name, file_path in video_files.items():
+        audio_path = os.path.join(output_folder, f"{file_name}.mp3")
+        converted_audio = convert_video_to_audio(file_path, audio_path)
+        
+        if converted_audio:
+            text = transcribe_audio(converted_audio, model)
+            save_transcription(file_name, text, output_folder)
+
+    print("Transcription process completed!")
